9512ba356c3f5d5cf2899fad8eeee6fbf520559c
Cydhra <j-hengstler@web.de>
2017-03-11 22:39:34 +0100

    Made session mutable

--- src/main/java/net/cydhra/nidhogg/data/Session.java
+++ src/main/java/net/cydhra/nidhogg/data/Session.java
@@ -5,11 +5,18 @@ import lombok.Data;
 import java.io.Serializable;
 
 /**
- *
+ * A Yggdrasil session with an access token, that is used to validate the session and a client token, that is used to identify the client
+ * that created the session
  */
 @Data
 public class Session implements Serializable {
-    private final String alias;
-    private final String accessToken;
-    private final String clientToken;
+    private String alias;
+    private String accessToken;
+    private String clientToken;
+    
+    public Session(final String alias, final String accessToken, final String clientToken) {
+        this.alias = alias;
+        this.accessToken = accessToken;
+        this.clientToken = clientToken;
+    }
 }
