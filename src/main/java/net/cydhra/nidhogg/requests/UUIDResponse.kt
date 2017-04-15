336f96b2baaef0ac0379fda1cb20a016dc39508e
Cydhra <ubezl@student.kit.edu>
2017-04-16 00:52:32 +0200

    Implemented UUIDResponse

---
+++ src/main/java/net/cydhra/nidhogg/requests/UUIDResponse.kt
@@ -0,0 +1,16 @@
+package net.cydhra.nidhogg.requests
+
+/**
+ * A response from Mojang API about a player's UUID
+ *
+ * [id] is the UUID requested
+ *
+ * [name] is the name of the UUID (not the requested name)
+ *
+ * [legacy] only appears when true - if the account is not migrated
+ *
+ * [demo] only appears when true - if the account hasn't bought an account
+ *
+ * see <a href="http://wiki.vg/Mojang_API#Username_-.3E_UUID_at_time">Mojang API</a>
+ */
+data class UUIDResponse(val id: String, val name: String, val legacy: Boolean?, val demo: Boolean?)
