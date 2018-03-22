5500102f881de081dd607c804576737d0ff9d882
Foorack <maxfaxalv@gmail.com>
2018-03-22 18:37:10 +0100

    Update Session.kt

--- src/main/java/net/cydhra/nidhogg/data/Session.kt
+++ src/main/java/net/cydhra/nidhogg/data/Session.kt
@@ -6,4 +6,4 @@ import java.io.Serializable
  * A Yggdrasil session with an access token, that is used to validate the session and a client token, that is used to identify the client
  * that created the session
  */
-data class Session(var alias: String, var accessToken: String, var clientToken: String) : Serializable
+data class Session(var id: String, var alias: String, var accessToken: String, var clientToken: String) : Serializable
