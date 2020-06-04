f172f6cf72cc06c6fbf5ece0dad2d4e5205ee02f
Johannes Hengstler <ubezl@student.kit.edu>
2020-06-05 01:10:50 +0200

    bumped version again, since I am starting to migrate anyway

---
+++ src/main/kotlin/net/cydhra/nidhogg/Nidhogg.kt
@@ -0,0 +1,5 @@
+package net.cydhra.nidhogg
+
+internal const val NIDHOGG_BRAND = "Nidhogg"
+internal const val NIDHOGG_VERSION = "2.0.0"
+internal const val NIDHOGG_USER_AGENT = "$NIDHOGG_BRAND/$NIDHOGG_VERSION"
