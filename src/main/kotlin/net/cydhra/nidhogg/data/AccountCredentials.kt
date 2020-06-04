b542e124ba9e52e11f2bb1d5241ce5fb8920d476
Johannes Hengstler <ubezl@student.kit.edu>
2020-06-05 00:38:15 +0200

    better documentation for AccountCredentials class

--- src/main/kotlin/net/cydhra/nidhogg/data/AccountCredentials.kt
+++ src/main/kotlin/net/cydhra/nidhogg/data/AccountCredentials.kt
@@ -3,9 +3,9 @@ package net.cydhra.nidhogg.data
 import java.io.Serializable
 
 /**
- * Mojang account credentials
+ * Mojang account credentials. Those are created by API users to use the authenticate function. This API never creates or stores instances of this class.
  *
- * @param username mojang account username
- * @param password mojang account password
+ * @param username Mojang account username
+ * @param password Mojang account password
  */
 data class AccountCredentials(val username: String, val password: String) : Serializable
