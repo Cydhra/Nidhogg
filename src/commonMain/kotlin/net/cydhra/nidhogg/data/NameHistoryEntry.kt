3981587cbd830053eaaf5b2ccd0d7b0ab5ebd6bd
Johannes Hengstler <ubezl@student.kit.edu>
2020-06-05 22:01:26 +0200

    migrated NameEntry

--- src/commonMain/kotlin/net/cydhra/nidhogg/data/NameHistoryEntry.kt
+++ src/commonMain/kotlin/net/cydhra/nidhogg/data/NameHistoryEntry.kt
@@ -1,9 +1,15 @@
 package net.cydhra.nidhogg.data
 
+import kotlinx.serialization.Serializable
+
 /**
  * An entry in the name history response. Each entry in the list represents one user name, the requested UUID was once associated with.
  *
  * @param name player name
  * @param changedToAt (optional) the timestamp in milliseconds when the name was changed to this entry
  */
-data class NameEntry(val name: String, val changedToAt: Long?)
+@Serializable
+data class NameHistoryEntry(
+        val name: String,
+        val changedToAt: Long? = null
+)
