91f71e3ebb2506f60b0bcbf8d09fd8296abcbed5
Johannes Hengstler <ubezl@student.kit.edu>
2020-06-05 19:45:14 +0200

    all of the profile parameters seem to be optional

--- src/commonMain/kotlin/net/cydhra/nidhogg/data/UserProfile.kt
+++ src/commonMain/kotlin/net/cydhra/nidhogg/data/UserProfile.kt
@@ -6,17 +6,18 @@ import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuidFrom
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 /**
  * The user profile associated to an account. It does not contain any data associated with Mojang games. See
- * [GameProfile] for that. The profile is returned by the Yggdrasil server during authentication, if requested.
+ * [GameProfile] for that. The profile is returned by the Yggdrasil server during authentication, if requested. All
+ * values except [id] and [username] may be null. I'm unaware under which conditions they are present.
  *
  * @param id the uuid of the user without hyphenation
- * @param email the email address of the user account. If the account is not migrated, this may contain a hash value.
+ * @param email probably the email address of the user account. If the account is not migrated, this may contain a hash
+ * value.
  * @param username the login name of the account. For migrated accounts this is the email address.
  * @param registerIp an IP address with the least significant byte censored. I am not exactly sure what IP address
- * this is supposed to be and why it is here.
+ * this is supposed to be and why it is here. I'm also unsure when exactly it is present, and when it is null.
  * @param migratedFrom a string describing where this account has been migrated from (if anywhere). Usually this is
  * set to "minecraft.net", if the account has been migrated from the legacy authentication system.
  * @param migrationDate timestamp of when the account has been migrated
@@ -40,28 +41,27 @@ import kotlinx.serialization.Transient
 @Serializable
 data class UserProfile(
         val id: String,
-        val email: String,
+        val email: String? = null,
         val username: String,
-        val registerIp: String,
-        val migratedFrom: String,
-        @SerialName("migratedAt") val migrationDate: Long,
-        @SerialName("registeredAt") val registrationDate: Long,
-        @SerialName("passwordChangedAt") val passwordChangeDate: Long,
-        val dateOfBirth: Long,
-        val suspended: Boolean,
-        val blocked: Boolean,
-        val secured: Boolean,
-        val migrated: Boolean,
-        val emailVerified: Boolean,
-        val legacyUser: Boolean,
-        val verifiedByParent: Boolean,
-        val properties: Array<ProfileProperty>
+        val registerIp: String? = null,
+        val migratedFrom: String? = null,
+        @SerialName("migratedAt") val migrationDate: Long? = null,
+        @SerialName("registeredAt") val registrationDate: Long? = null,
+        @SerialName("passwordChangedAt") val passwordChangeDate: Long? = null,
+        val dateOfBirth: Long? = null,
+        val suspended: Boolean? = null,
+        val blocked: Boolean? = null,
+        val secured: Boolean? = null,
+        val migrated: Boolean? = null,
+        val emailVerified: Boolean? = null,
+        val legacyUser: Boolean? = null,
+        val verifiedByParent: Boolean? = null,
+        val properties: Array<ProfileProperty>? = null
 ) {
     /**
      * Generate an instance of [Uuid] from the player [id]. The [Uuid] instance is generated lazily and will be
      * stored afterwards.
      */
-    @Transient
     val uuid: Uuid by lazy {
         uuidFrom("${id.subSequence(0, 8)}-${id.subSequence(8, 12)}-${id.subSequence(12, 16)}" +
                 "-${id.subSequence(16, 20)}-${id.subSequence(20, 32)}"
